58d62141d517a6392ba2667abb712aff6bcb8740
Santiago <santiago@Santiagos-MacBook-Pro.local>
2025-10-30 20:56:22 +0700

    fourth commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 
 export default function LoginPage() {
   const handleGoogleLogin = async () => {
